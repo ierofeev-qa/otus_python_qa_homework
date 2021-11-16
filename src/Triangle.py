534f786d0d83ed4460035e9562bbf1defb7ad78b
ierofeev <ivanerofeev@bk.ru>
2021-11-16 22:13:24 +0300

    Refactor code

--- src/Triangle.py
+++ src/Triangle.py
@@ -5,33 +5,30 @@ from src.Figure import Figure
 class Triangle(Figure):
     name = "Triangle"
 
-    def __init__(self, side_a, side_b, side_c):
+    def __init__(self, side_one, side_two, side_three):
         super(Figure, self).__init__()
-        self.__side_a = side_a
-        self.__side_b = side_b
-        self.__side_c = side_c
+        self.__side_one = side_one
+        self.__side_two = side_two
+        self.__side_three = side_three
 
     @staticmethod
-    def triangle_is_exist(side_a, side_b, side_c):
-        first_condition = side_a + side_b > side_c
-        second_condition = side_a + side_c > side_b
-        third_condition = side_b + side_c > side_a
-        return first_condition and second_condition and third_condition
+    def triangle_is_exist(side_one, side_two, side_three):
+        return side_one + side_two > side_three and side_one + side_three > side_two and side_two + side_three > side_one
 
-    def __new__(cls, side_a, side_b, side_c):
-        if cls.triangle_is_exist(side_a, side_b, side_c):
+    def __new__(cls, side_one, side_two, side_three):
+        if cls.triangle_is_exist(side_one, side_two, side_three):
             instance = super(Triangle, cls).__new__(cls)
-            instance.side_one, instance.side_two, instance.side_three = side_a, side_b, side_c
+            instance.side_one, instance.side_two, instance.side_three = side_one, side_two, side_three
             return instance
         return None
 
     @property
     def area(self):
-        semi_per = (self.__side_a + self.__side_b + self.__side_c) / 2
+        semi_per = (self.__side_one + self.__side_two + self.__side_three) / 2
         return math.sqrt(
-            semi_per * (semi_per - self.__side_a) * (semi_per - self.__side_b) * (semi_per - self.__side_c)
+            semi_per * (semi_per - self.__side_one) * (semi_per - self.__side_two) * (semi_per - self.__side_three)
         )
 
     @property
     def perimeter(self):
-        return self.__side_a + self.__side_b + self.__side_c
+        return self.__side_one + self.__side_two + self.__side_three
